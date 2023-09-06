1214df3cf0379a99eb8724db14e2ac51a6a82851
Gururaj Deshpande <66385690+guru-desh@users.noreply.github.com>
2023-09-06 16:29:34 -0400

    add plotly method

--- plot_data.py
+++ plot_data.py
@@ -1,6 +1,7 @@
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 
 def plot_data(data):
@@ -11,5 +12,22 @@ def plot_data(data):
     plt.legend(loc='upper right')
     plt.show()
 
+
+def plot_data_plotly(data, title=None):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=[row[0] for row in data], y=[row[1] for row in data],
+                        mode='lines',
+                        name='x'))
+    fig.add_trace(go.Scatter(x=[row[0] for row in data], y=[row[2] for row in data],
+                        mode='lines',
+                        name='y'))
+    fig.add_trace(go.Scatter(x=[row[0] for row in data], y=[row[3] for row in data],
+                        mode='lines',
+                        name='z'))
+    fig.update_layout(
+        title=title,
+        xaxis_title="Time",
+        yaxis_title="Acceleration"
+    )
 
 # plot_data("test.csv")
